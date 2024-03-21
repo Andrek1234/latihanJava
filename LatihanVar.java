f9749ac1b9ef399b6d018fbdf63820a9bdc2c0cd
Andrek1234 <andre.kurniawan.1144@gmail.com>
2024-03-21 16:52:51 +0700

    Latihan Java

---
+++ LatihanVar.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanvar;
+
+/**
+ *
+ * @author ACER
+ */
+public class LatihanVar {
+
+    public static void main(String[] args) {
+        // code aplecation
+        int JariJari;
+        double Keliling;
+        final double pi = 3.14;
+        JariJari = 30;
+        Keliling = 2*pi*JariJari;
+        System.out.println("Keliling Lingkaran : " + Keliling);
+        System.out.println("=========");
+    }
+}
