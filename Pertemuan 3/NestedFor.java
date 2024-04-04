c3b552ffccfdf113d28d99b47af88b3f2b623bb1
Andrek1234 <andre.kurniawan.1144@gmail.com>
2024-04-04 15:42:50 +0700

    Codingan P3

--- Pertemuan 3/NestedFor.java
+++ Pertemuan 3/NestedFor.java
@@ -13,9 +13,9 @@ public class NestedFor {
     public static void main(String[] args) {
         int a, b;
         for (a = 1; a <= 5; a++){
-            System.out.println(a);
+            System.out.println();
             for (b = a; b <= 5; b++){
-                System.out.println(b);
+                System.out.print(a);
             }
             System.out.println("\n");
         }
