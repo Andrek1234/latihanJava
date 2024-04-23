1b5e18ff803626cec9c956908a1fbe4b73d934fc
Andrek1234 <andre.kurniawan.1144@gmail.com>
2024-04-23 08:58:40 +0700

    Kasus slide 6

---
+++ Pertemuan 5/Latihan1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author ACER
+ */
+import javax.swing.*;
+class Latihan1 {
+
+    public static void main(String[] args) {
+        JFrame myWindow;
+        
+        myWindow = new JFrame ();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("My First Java Program");
+        myWindow.setVisible(true);
+    }
+}
